6b2d9d1e5d05824ce8c0d859e90e3bc8cf012933
aozertsov <aozertsov@gmail.com>
2021-08-10 17:14:24 +0400

    [editor] check if xml file is root

--- JetBrains/xml-visualizer/src/main/java/io/aozertsov/xml/editor/XmlVisualEditorProvider.java
+++ JetBrains/xml-visualizer/src/main/java/io/aozertsov/xml/editor/XmlVisualEditorProvider.java
@@ -10,6 +10,7 @@ import com.intellij.util.xml.ui.DomFileEditor;
 import com.intellij.util.xml.ui.PerspectiveFileEditor;
 import com.intellij.util.xml.ui.PerspectiveFileEditorProvider;
 import io.aozertsov.xml.dom.Root;
+import io.aozertsov.xml.util.XmlHelper;
 import org.jetbrains.annotations.NotNull;
 
 public class XmlVisualEditorProvider extends PerspectiveFileEditorProvider {
@@ -27,6 +28,12 @@ public class XmlVisualEditorProvider extends PerspectiveFileEditorProvider {
 
     @Override
     public boolean accept(@NotNull Project project, @NotNull VirtualFile file) {
-        return true;
+        if (XmlHelper.isXmlFile(project, file)) {
+            DomManager domManager = DomManager.getDomManager(project);
+            PsiFile psiFile = PsiManager.getInstance(project).findFile(file);
+            var fileElement = domManager.getFileElement((XmlFile) psiFile, Root.class);
+            return fileElement != null;
+        }
+        return false;
     }
 }
