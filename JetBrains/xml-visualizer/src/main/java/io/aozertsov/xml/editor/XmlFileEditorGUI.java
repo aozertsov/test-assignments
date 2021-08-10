74876cb49d853925664feed0f6ce2dcc707b2ed3
aozertsov <aozertsov@gmail.com>
2021-08-10 18:35:17 +0400

    [editor] use tree to show file structure

--- JetBrains/xml-visualizer/src/main/java/io/aozertsov/xml/editor/XmlFileEditorGUI.java
+++ JetBrains/xml-visualizer/src/main/java/io/aozertsov/xml/editor/XmlFileEditorGUI.java
@@ -1,20 +1,24 @@
 package io.aozertsov.xml.editor;
 
 import com.intellij.util.xml.ui.BasicDomElementComponent;
+import io.aozertsov.xml.dom.Node;
 import io.aozertsov.xml.dom.Root;
 
 import javax.swing.*;
 import javax.swing.event.TreeSelectionEvent;
 import javax.swing.event.TreeSelectionListener;
+import javax.swing.tree.DefaultMutableTreeNode;
+import javax.swing.tree.DefaultTreeModel;
+import java.util.List;
 
 public class XmlFileEditorGUI extends BasicDomElementComponent<Root> implements TreeSelectionListener {
 
     private JPanel editorWindowContent;
-    private JTextPane textPane;
+    private JTree rootTree;
 
     public XmlFileEditorGUI(Root domElement) {
         super(domElement);
-        textPane.setText("Init component");
+        initTree();
     }
 
     @Override
@@ -25,5 +29,21 @@ public class XmlFileEditorGUI extends BasicDomElementComponent<Root> implements 
     @Override
     public void valueChanged(TreeSelectionEvent e) {
 
+    }
+
+    private void initTree() {
+        DefaultMutableTreeNode top = new DefaultMutableTreeNode();
+        createNodes(top, getDomElement().getNodes());
+        rootTree.setModel(new DefaultTreeModel(top));
+        rootTree.setRootVisible(false);
+        rootTree.setCellRenderer(new XmlRenderer());
+    }
+
+    private void createNodes(DefaultMutableTreeNode treeNode, List<Node> nodes) {
+        for (Node node : nodes) {
+            DefaultMutableTreeNode entityNode = new DefaultMutableTreeNode(node);
+            treeNode.add(entityNode);
+            createNodes(entityNode, node.getChildren());
+        }
     }
 }
